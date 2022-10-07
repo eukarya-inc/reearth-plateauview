7cc43c4054f79153813ab00502cfe7a4420b5648
KaWaite <kyle.waite@outlook.com>
2022-10-07 15:56:44 +0900

    refactor map settings, share functionality

--- plugin/web/extensions/sidebar/components/content/Share.tsx
+++ plugin/web/extensions/sidebar/components/content/Share.tsx
@@ -1,36 +1,105 @@
 import { Input, Row, Icon } from "@web/extensions/sharedComponents";
 import CommonPage from "@web/extensions/sidebar/components/content/CommonPage";
+import { usePublishUrl } from "@web/extensions/sidebar/state";
+import { postMsg } from "@web/extensions/sidebar/utils";
 import { styled } from "@web/theme";
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useCallback, useEffect } from "react";
+
+function makeUrlSuffix() {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 15; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function generatePrintView(payload?: string) {
+  const doc = window.open()?.document;
+
+  if (!doc || !payload) return;
+
+  const iframeHTML = `
+    <div style="display: flex; flex-direction: column; max-width: 1200px; height: 100%; margin: 0 auto; padding: 20px;">
+      <div style="display: flex; justify-content: right; align-items: center; gap: 8px; height: 60px;">
+        <button style="padding: 8px; border: none; border-radius: 4px; background: #00BEBE; color: white; cursor: pointer;">Download map</button>
+        <button style="padding: 9px; border: none; border-radius: 4px; background: #00BEBE; color: white; cursor: pointer;">Print</button>
+      </div>
+      <div style="display: flex; justify-content: center; width: 100%;">
+        <img src="${payload}" style="max-width: 100%; object-fit: contain;" />
+      </div>
+      <div>
+        <p>This map was created using https://plateauview.mlit.go.jp on ${new Date()}</p>
+      </div>
+    </div>
+`;
+
+  const css = `html,body{ margin: 0; }`;
+
+  const styleTag = doc.createElement("style");
+  styleTag.appendChild(document.createTextNode(css));
+  styleTag.setAttribute("type", "text/css");
+  doc.head.appendChild(styleTag);
+
+  const iframe = doc.createElement("iframe");
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+
+  doc.body.appendChild(iframe);
+
+  iframe.contentWindow?.document.open();
+  iframe.contentWindow?.document.write(iframeHTML);
+
+  const iframeHtmlStyle = iframe.contentWindow?.document.createElement("style");
+  if (iframeHtmlStyle) {
+    iframeHtmlStyle.appendChild(document.createTextNode(css));
+    iframeHtmlStyle.setAttribute("type", "text/css");
+    iframe.contentWindow?.document.head.appendChild(iframeHtmlStyle);
+  }
+  iframe.contentWindow?.document.close();
+  console.log(iframe, "iframe");
+  return iframe;
+}
 
 const Share: React.FC = () => {
-  const [publicUrl, setPublicUrl] = useState<string>();
-
-  // TO DO: handle url generation. BELOW IS TEMPORARY... PROBABLY
-  function makeUrlSuffix() {
-    let result = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (let i = 0; i < 15; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    console.log(result, "result");
-    return result;
-  }
-
-  const handleUrlGeneration = useCallback(() => {
-    console.log("hey");
-    const suffix = makeUrlSuffix();
-    setPublicUrl(`https://plateauview.mlit.go.jp/${suffix}`);
-  }, []);
+  const [publishUrl, setPublishUrl] = usePublishUrl();
 
   useEffect(() => {
-    handleUrlGeneration();
-  }, [handleUrlGeneration]);
-  // TO DO: handle url generation. ABOVE IS TEMPORARY... PROBABLY
+    const suffix = makeUrlSuffix();
+    if (!publishUrl) {
+      // To do: get available url from PLATEAU backend and set that here,
+      // OR need to add a regen if we get an error on the copy/publish button
+      setPublishUrl(`https://plateauview.mlit.go.jp/${suffix}`);
+    }
+  }, [publishUrl, setPublishUrl]);
 
   // TO DO: handle screenshot (download/show print view) functionality
-  // NEEDS API
+  const handleScreenshotShow = useCallback(() => {
+    postMsg({ action: "screenshot" });
+  }, []);
+
+  const handleScreenshotSave = useCallback(() => {
+    postMsg({ action: "screenshot-save" });
+  }, []);
+
+  addEventListener("message", e => {
+    if (e.source !== parent) return;
+    if (e.data.type) {
+      if (e.data.type === "screenshot") {
+        console.log(e.data, "dataaaaaaaaaa");
+        generatePrintView(e.data.payload);
+      } else if (e.data.type === "screenshot-save") {
+        console.log(e.data, "dataaaaaaaaaa saveeeeeee");
+        const link = document.createElement("a");
+        link.download = "screenshot.png";
+        link.href = e.data.payload;
+        link.click();
+        link.remove();
+      }
+    }
+  });
 
   return (
     <CommonPage title="共有・印刷">
@@ -38,7 +107,7 @@ const Share: React.FC = () => {
         <Subtitle>URLで共有</Subtitle>
         <InputGroup>
           <FlexWrapper>
-            <Input value={publicUrl} />
+            <Input value={publishUrl} />
             <StyledButton>
               <Icon icon="copy" />
             </StyledButton>
@@ -48,7 +117,7 @@ const Share: React.FC = () => {
         <Subtitle>HTMLページへの埋め込みは下記のコードをお使いください：</Subtitle>
         <InputGroup>
           <FlexWrapper>
-            <Input value={`<iframe src=${publicUrl} />`} />
+            <Input value={`<iframe src=${publishUrl} />`} />
             <StyledButton>
               <Icon icon="copy" />
             </StyledButton>
@@ -60,8 +129,8 @@ const Share: React.FC = () => {
         <Subtitle>印刷</Subtitle>
         <SectionWrapper>
           <ButtonWrapper>
-            <Button onClick={() => {}}>Download map (png)</Button>
-            <Button onClick={() => {}}>Show Print View</Button>
+            <Button onClick={handleScreenshotSave}>Download map (png)</Button>
+            <Button onClick={handleScreenshotShow}>Show Print View</Button>
           </ButtonWrapper>
           <SubText>このマップを印刷できる状態で表示</SubText>
         </SectionWrapper>
