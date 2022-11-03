9ddcb60ad89685bed2a5ae122c8df0cbea8064aa
KaWaite <kyle.waite@outlook.com>
2022-11-03 12:07:21 +0900

    keep event listeners separate

--- plugin/web/extensions/sidebar/core/globalHooks.ts
+++ plugin/web/extensions/sidebar/core/globalHooks.ts
@@ -85,14 +85,6 @@ addEventListener("message", e => {
   if (e.data.type) {
     if (e.data.type === "extended") {
       updateExtended(e.data.payload);
-    } else if (e.data.type === "screenshot") {
-      generatePrintView(e.data.payload);
-    } else if (e.data.type === "screenshot-save") {
-      const link = document.createElement("a");
-      link.download = "screenshot.png";
-      link.href = e.data.payload;
-      link.click();
-      link.remove();
     }
   }
 });
@@ -111,56 +103,4 @@ function updateExtended(e: { vertically: boolean }) {
     body?.classList.remove("extended");
     root?.classList.remove("extended");
   }
-}
-
-function generatePrintView(payload?: string) {
-  const doc = window.open()?.document;
-
-  if (!doc || !payload) return;
-
-  const css = `html,body{ margin: 0; }`;
-
-  const styleTag = doc.createElement("style");
-  styleTag.appendChild(document.createTextNode(css));
-  styleTag.setAttribute("type", "text/css");
-  doc.head.appendChild(styleTag);
-
-  const iframe = doc.createElement("iframe");
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.style.border = "none";
-
-  doc.body.appendChild(iframe);
-
-  const iframeDoc = iframe.contentWindow?.document;
-  if (!iframeDoc) return;
-
-  iframeDoc.open();
-
-  const iframeHTML = `
-  <div style="display: flex; flex-direction: column; max-width: 1200px; height: 100%; margin: 0 auto; padding: 20px;">
-    <div style="display: flex; justify-content: right; align-items: center; gap: 8px; height: 60px;">
-      <button style="padding: 8px; border: none; border-radius: 4px; background: #00BEBE; color: white; cursor: pointer;">Download map</button>
-      <button style="padding: 9px; border: none; border-radius: 4px; background: #00BEBE; color: white; cursor: pointer;">Print</button>
-    </div>
-    <div style="display: flex; justify-content: center; width: 100%;">
-      <img src="${payload}" style="max-width: 100%; object-fit: contain;" />
-    </div>
-    <div>
-      <p>This map was created using https://plateauview.mlit.go.jp on ${new Date()}</p>
-    </div>
-  </div>
-`;
-  iframe.contentWindow?.document.write(iframeHTML);
-
-  const iframeHtmlStyle = iframe.contentWindow?.document.createElement("style");
-  if (iframeHtmlStyle) {
-    iframeHtmlStyle.appendChild(document.createTextNode(css));
-    iframeHtmlStyle.setAttribute("type", "text/css");
-    iframe.contentWindow?.document.head.appendChild(iframeHtmlStyle);
-  }
-
-  iframe.contentWindow?.document.close();
-
-  return iframe;
 }
