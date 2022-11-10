7e9acfa585daaabf348b6cdafe277a91d78fce24
nina992 <nouralali992@gmail.com>
2022-11-10 19:23:57 +0300

    pr-fix2

--- plugin/web/extensions/location/hook.ts
+++ plugin/web/extensions/location/hook.ts
@@ -2,14 +2,9 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 
 import { useShowGoogleModal } from "./state";
 import { MouseEventData } from "./types";
-import { postMsg } from "./utils";
+import { postMsg, distances } from "./utils";
 
 export default () => {
-  const distances = [
-    1, 2, 3, 5, 10, 20, 30, 50, 100, 200, 300, 500, 1000, 2000, 3000, 5000, 10000, 20000, 30000,
-    50000, 100000, 200000, 300000, 500000, 1000000, 2000000, 3000000, 5000000, 10000000, 20000000,
-    30000000, 50000000,
-  ];
   let pixelDistance = 0;
 
   const [currentPoint, setCurrentPoint] = useState<MouseEventData>();
