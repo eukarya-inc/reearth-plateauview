97f705a780e7ca00f8e8ec814f3a860f80574303
nina992 <nouralali992@gmail.com>
2022-11-14 11:30:10 +0300

    spaces-delete-state

--- plugin/web/extensions/location/state/index.ts
+++
@@ -1,7 +0,0 @@
-import { atom, useAtom } from "jotai";
-
-const showGoogleModal = atom<boolean>(false);
-export const useShowGoogleModal = () => useAtom(showGoogleModal);
-
-const showTerrainModal = atom<boolean>(false);
-export const useShowTerrainModal = () => useAtom(showTerrainModal);
