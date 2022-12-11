fb58445d6e26cddd1b240d0f31a95faab980ac14
nina992 <nouralali992@gmail.com>
2022-12-12 01:41:03 +0300

    init

--- plugin/web/extensions/geolocation/core/hooks.ts
+++ plugin/web/extensions/geolocation/core/hooks.ts
@@ -1,27 +1,44 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 
 import { CurrentLocationInfo } from "./types";
 import { postMsg } from "./utils";
 
+const initialLocation: CurrentLocationInfo = {
+  latitude: 5.70249,
+  longitude: 39.7622,
+  altitude: 5000,
+};
+
+const goSuccess = (position: GeolocationPosition) => {
+  return {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+    altitude: position.coords.altitude ?? 5000,
+  };
+};
+
+const goError = (err: GeolocationPositionError) => {
+  console.error("Error Code = " + err.code + " - " + err.message);
+  return initialLocation;
+};
+
+const getPosition = () =>
+  new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(
+      pos => resolve(goSuccess(pos)),
+      err => reject(goError(err)),
+    ),
+  );
 export default () => {
-  const [currentLocation, setCurrentLocation] = useState<CurrentLocationInfo>();
+  let currentLocation: CurrentLocationInfo;
 
   const handleFlyToCurrentLocation = useCallback(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(
-        function (position) {
-          setCurrentLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            altitude: position.coords.altitude ?? 5000,
-          });
-        },
-        function (error) {
-          console.error("Error Code = " + error.code + " - " + error.message);
-        },
-      );
-    }
-    postMsg({ action: "flyTo", payload: { currentLocation } });
+    getPosition()
+      .then((position: CurrentLocationInfo | any) => {
+        currentLocation = { ...position };
+        postMsg({ action: "flyTo", payload: { currentLocation } });
+      })
+      .catch(error => console.error("error:", error.message));
   }, []);
 
   return {
