9971a0f7a4dd8f2809f5140d95875180aa956b2e
nina992 <nouralali992@gmail.com>
2022-12-21 18:01:08 +0300

    fix6

--- plugin/vite-configs/sidebar/popups/clipFunction/web.ts
+++ plugin/vite-configs/sidebar/popups/clipFunction/web.ts
@@ -6,4 +6,4 @@ import { defineConfig } from "vite";
 import { web } from "../../../../vite.config.template";
 
 // https://vitejs.dev/config/
-export default defineConfig(web({ name: "ClipFunction", parent: "sidebar", type: "popup" }));
+export default defineConfig(web({ name: "clipFunction", parent: "sidebar", type: "popup" }));
