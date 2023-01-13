f453fa51edfea1c6c76df8c2f0b3a81ac905df7d
rot1024 <aayhrot@gmail.com>
2023-01-13 15:58:23 +0900

    add webhook handlers

---
+++ server/sdk/webhook.go
@@ -0,0 +1,47 @@
+package sdk
+
+import (
+	"net/http"
+
+	"github.com/eukarya-inc/reearth-plateauview/server/cms/cmswebhook"
+	"github.com/reearth/reearthx/log"
+)
+
+var (
+	modelKey = "plateau"
+)
+
+func WebhookHandler(conf Config) (cmswebhook.Handler, error) {
+	// c, err := NewServices(conf)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	return func(req *http.Request, w *cmswebhook.Payload) error {
+		if !w.Operator.IsUser() {
+			log.Infof("geospatialjp webhook: invalid event operator: %+v", w.Operator)
+			return nil
+		}
+
+		if w.Type != "item.update" && w.Type != "item.publish" {
+			log.Infof("geospatialjp webhook: invalid event type: %s", w.Type)
+			return nil
+		}
+
+		if w.Data.Item == nil || w.Data.Model == nil {
+			log.Infof("geospatialjp webhook: invalid event data: %+v", w.Data)
+			return nil
+		}
+
+		if w.Data.Model.Key != modelKey {
+			log.Infof("geospatialjp webhook: invalid model id: %s, key: %s", w.Data.Item.ModelID, w.Data.Model.Key)
+			return nil
+		}
+
+		// ctx := req.Context()
+		// item := ItemFrom(w.Data.Item)
+
+		// TODO
+		return nil
+	}, nil
+}
---
+++ server/searchindex/common.go
@@ -0,0 +1,3 @@
+package searchindex
+
+type Config struct{}
