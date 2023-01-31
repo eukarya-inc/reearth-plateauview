fc440ce677829d9841180c5c267e5c9076fc90ce
gop50k <nakagawago0614@gmail.com>
2023-01-31 17:32:12 +0900

    feat(server): move echo

--- server/visualizer/service.go
+++ server/visualizer/service.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 
 	"github.com/eukarya-inc/reearth-plateauview/server/cms"
+	"github.com/eukarya-inc/reearth-plateauview/server/cmsintegration"
+	"github.com/eukarya-inc/reearth-plateauview/server/opinion"
+	"github.com/eukarya-inc/reearth-plateauview/server/sdk"
+	"github.com/eukarya-inc/reearth-plateauview/server/share"
+	"github.com/labstack/echo"
+	"github.com/samber/lo"
 )
 
 type Config struct {
@@ -25,6 +31,13 @@ func NewServices(c Config) (s Services, _ error) {
 		return Services{}, fmt.Errorf("failed to init cms: %w", err)
 	}
 	s.CMS = cms
+
+	e := echo.New()
+	e.POST("/notify_fme", lo.Must(cmsintegration.NotifyHandler(conf.CMSIntegration())))
+	e.POST("/notify_sdk", lo.Must(sdk.NotifyHandler(conf.SDK())))
+	lo.Must0(visualizer.Echo(e.Group(""), conf.Visualizer()))
+	lo.Must0(share.Echo(e.Group("/share"), conf.Share()))
+	opinion.Echo(e.Group("/opinion"), conf.Opinion())
 
 	return
 }
