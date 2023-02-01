fd58b29d30b9cdd7373fce030abf6d1536799fe5
KaWaite <kyle.waite@outlook.com>
2023-02-01 16:40:29 +0900

    hide unfinished fields

--- plugin/web/extensions/sidebar/core/components/content/common/DatasetCard/hooks.ts
+++ plugin/web/extensions/sidebar/core/components/content/common/DatasetCard/hooks.ts
@@ -52,100 +52,100 @@ export default ({
     },
   };
 
-  const pointFields: {
-    [key: string]: { name: string; onClick?: (property: any) => void };
-  } = {
-    camera: {
-      name: "カメラ",
-      onClick: () =>
-        handleAddField({
-          position: {
-            lng: 0,
-            lat: 0,
-            height: 0,
-            pitch: 0,
-            heading: 0,
-            roll: 0,
-          },
-        }),
-    },
-  };
+  //   const pointFields: {
+  //     [key: string]: { name: string; onClick?: (property: any) => void };
+  //   } = {
+  //     camera: {
+  //       name: "カメラ",
+  //       onClick: () =>
+  //         handleAddField({
+  //           position: {
+  //             lng: 0,
+  //             lat: 0,
+  //             height: 0,
+  //             pitch: 0,
+  //             heading: 0,
+  //             roll: 0,
+  //           },
+  //         }),
+  //     },
+  //   };
 
-  const polylineFields: {
-    [key: string]: { name: string; onClick?: (property: any) => void };
-  } = {
-    camera: {
-      name: "カメラ",
-      onClick: () =>
-        handleAddField({
-          position: {
-            lng: 0,
-            lat: 0,
-            height: 0,
-            pitch: 0,
-            heading: 0,
-            roll: 0,
-          },
-        }),
-    },
-  };
+  //   const polylineFields: {
+  //     [key: string]: { name: string; onClick?: (property: any) => void };
+  //   } = {
+  //     camera: {
+  //       name: "カメラ",
+  //       onClick: () =>
+  //         handleAddField({
+  //           position: {
+  //             lng: 0,
+  //             lat: 0,
+  //             height: 0,
+  //             pitch: 0,
+  //             heading: 0,
+  //             roll: 0,
+  //           },
+  //         }),
+  //     },
+  //   };
 
-  const polygonFields: {
-    [key: string]: { name: string; onClick?: (property: any) => void };
-  } = {
-    camera: {
-      name: "カメラ",
-      onClick: () =>
-        handleAddField({
-          position: {
-            lng: 0,
-            lat: 0,
-            height: 0,
-            pitch: 0,
-            heading: 0,
-            roll: 0,
-          },
-        }),
-    },
-  };
+  //   const polygonFields: {
+  //     [key: string]: { name: string; onClick?: (property: any) => void };
+  //   } = {
+  //     camera: {
+  //       name: "カメラ",
+  //       onClick: () =>
+  //         handleAddField({
+  //           position: {
+  //             lng: 0,
+  //             lat: 0,
+  //             height: 0,
+  //             pitch: 0,
+  //             heading: 0,
+  //             roll: 0,
+  //           },
+  //         }),
+  //     },
+  //   };
 
-  const ThreeDModelFields: {
-    [key: string]: { name: string; onClick?: (property: any) => void };
-  } = {
-    camera: {
-      name: "カメラ",
-      onClick: () =>
-        handleAddField({
-          position: {
-            lng: 0,
-            lat: 0,
-            height: 0,
-            pitch: 0,
-            heading: 0,
-            roll: 0,
-          },
-        }),
-    },
-  };
+  //   const ThreeDModelFields: {
+  //     [key: string]: { name: string; onClick?: (property: any) => void };
+  //   } = {
+  //     camera: {
+  //       name: "カメラ",
+  //       onClick: () =>
+  //         handleAddField({
+  //           position: {
+  //             lng: 0,
+  //             lat: 0,
+  //             height: 0,
+  //             pitch: 0,
+  //             heading: 0,
+  //             roll: 0,
+  //           },
+  //         }),
+  //     },
+  //   };
 
-  const ThreeDTileFields: {
-    [key: string]: { name: string; onClick?: (property: any) => void };
-  } = {
-    camera: {
-      name: "カメラ",
-      onClick: () =>
-        handleAddField({
-          position: {
-            lng: 0,
-            lat: 0,
-            height: 0,
-            pitch: 0,
-            heading: 0,
-            roll: 0,
-          },
-        }),
-    },
-  };
+  //   const ThreeDTileFields: {
+  //     [key: string]: { name: string; onClick?: (property: any) => void };
+  //   } = {
+  //     camera: {
+  //       name: "カメラ",
+  //       onClick: () =>
+  //         handleAddField({
+  //           position: {
+  //             lng: 0,
+  //             lat: 0,
+  //             height: 0,
+  //             pitch: 0,
+  //             heading: 0,
+  //             roll: 0,
+  //           },
+  //         }),
+  //     },
+  //   };
 
   const fieldGroups: {
     [key: string]: {
@@ -157,14 +157,14 @@ export default ({
       name: "一般",
       fields: generalFields,
     },
-    point: {
-      name: "ポイント",
-      fields: pointFields,
-    },
-    polyline: { name: "ポリライン", fields: polylineFields },
-    polygone: { name: "ポリゴン", fields: polygonFields },
-    "3d-model": { name: "3Dモデル", fields: ThreeDModelFields },
-    "3d-tile": { name: "3Dタイル", fields: ThreeDTileFields },
+    // point: {
+    //   name: "ポイント",
+    //   fields: pointFields,
+    // },
+    // polyline: { name: "ポリライン", fields: polylineFields },
+    // polygone: { name: "ポリゴン", fields: polygonFields },
+    // "3d-model": { name: "3Dモデル", fields: ThreeDModelFields },
+    // "3d-tile": { name: "3Dタイル", fields: ThreeDTileFields },
   };
   return {
     fieldGroups,
