44addfa3970739f20717d5995c1ab9a185e54b25
nourbalaha <nourbalaha0@gmail.com>
2023-02-16 22:42:31 +0900

    wip condition field

--- plugin/web/extensions/sidebar/core/components/content/common/DatasetCard/Field/Fields/point/common/ConditionField.tsx
+++ plugin/web/extensions/sidebar/core/components/content/common/DatasetCard/Field/Fields/point/common/ConditionField.tsx
@@ -1,60 +1,60 @@
-import { Icon, Dropdown, Menu } from "@web/sharedComponents";
+import { Select, InputNumber } from "@web/sharedComponents";
 import { styled } from "@web/theme";
 
 import { Cond } from "../../types";
 import { FieldTitle, FieldValue, FieldWrapper } from "../commonComponents";
 
-const operators: { [key: string]: string } = {
-  greater: ">",
-  less: "<",
-  greaterEqual: ">=",
-  lessEqual: "<=",
-  equal: "=",
-};
+const operatorOptions = [
+  { value: "greater", label: ">" },
+  { value: "less", label: "<" },
+  { value: "greaterEqual", label: ">=" },
+  { value: "lessEqual", label: "<=" },
+  { value: "equal", label: "=" },
+];
+
+const operandOptions = [
+  { value: "height", label: "Height" },
+  { value: "Width", label: "Width" },
+];
 
 type Props = {
   title: string;
   fieldGap?: number;
   condition: Cond<any>;
+  onChange?: (condition: Cond<any>) => void;
 };
 
-const ConditionField: React.FC<Props> = ({ title, fieldGap, condition }) => {
-  const menu = (
-    <Menu
-      items={Object.keys(operators).map(op => {
-        return {
-          key: op,
-          label: (
-            <p style={{ margin: 0 }} onClick={() => console.log(op)}>
-              {operators[op]}
-            </p>
-          ),
-        };
-      })}
-    />
-  );
+const ConditionField: React.FC<Props> = ({ title, fieldGap, condition, onChange }) => {
+  const handleOperandChange = (value: any) => {
+    const cond = { ...condition, value };
+    onChange?.(cond);
+  };
+
+  const handleOperatorChange = (value: any) => {
+    const cond = { ...condition, value };
+    onChange?.(cond);
+  };
+
+  const handleValueChange = (value: any) => {
+    const cond = { ...condition, value };
+    onChange?.(cond);
+  };
 
   return (
     <FieldWrapper gap={fieldGap}>
       <FieldTitle>{title}</FieldTitle>
-      <FieldValue>
-        <Dropdown overlay={menu} placement="bottom" trigger={["click"]}>
-          <StyledDropdownButton>
-            <p style={{ margin: 0 }}>{condition.operand}</p>
-            <Icon icon="arrowDownSimple" size={12} />
-          </StyledDropdownButton>
-        </Dropdown>
+      <FieldValue noBorder>
+        <Select options={operandOptions} style={{ width: "100%" }} onChange={handleOperandChange} />
+      </FieldValue>
+      <FieldValue noBorder>
+        <Select
+          options={operatorOptions}
+          style={{ width: "100%" }}
+          onChange={handleOperatorChange}
+        />
       </FieldValue>
       <FieldValue>
-        <Dropdown overlay={menu} placement="bottom" trigger={["click"]}>
-          <StyledDropdownButton>
-            <p style={{ margin: 0 }}>{condition.operator}</p>
-            <Icon icon="arrowDownSimple" size={12} />
-          </StyledDropdownButton>
-        </Dropdown>
-      </FieldValue>
-      <FieldValue>
-        <NumberInput value={condition.value} />
+        <NumberInput value={condition.value} onChange={handleValueChange} />
       </FieldValue>
     </FieldWrapper>
   );
@@ -62,7 +62,7 @@ const ConditionField: React.FC<Props> = ({ title, fieldGap, condition }) => {
 
 export default ConditionField;
 
-const NumberInput = styled.input.attrs({ type: "number" })`
+const NumberInput = styled(InputNumber)`
   height: 100%;
   width: 100%;
   flex: 1;
@@ -73,14 +73,4 @@ const NumberInput = styled.input.attrs({ type: "number" })`
   :focus {
     border: none;
   }
-`;
-
-const StyledDropdownButton = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  align-content: center;
-  padding: 0 16px;
-  cursor: pointer;
 `;
