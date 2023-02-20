fb8a42b7bf86ce345c56780dc411465848608ab4
keiya01 <keiya.s.0210@gmail.com>
2023-02-20 10:19:32 +0900

    fix: default condition

--- plugin/web/extensions/sidebar/core/components/content/common/DatasetCard/Field/Fields/3dtiles/BuildingColor/conditions.ts
+++ plugin/web/extensions/sidebar/core/components/content/common/DatasetCard/Field/Fields/3dtiles/BuildingColor/conditions.ts
@@ -15,6 +15,8 @@ const equalNumber = (prop: string, value: number) => `(\${${prop}} === ${value})
 
 type Condition = [condition: string, result: string];
 
+const DEFAULT_CONDITION: Condition = ["true", "color('white')"];
+
 const conditionalHeight = defaultConditionalNumber("計測高さ");
 const HEIGHT_CONDITIONS: Condition[] = [
   [compareGreaterThan(conditionalHeight, 180), "color('#F7FF00')"],
@@ -23,6 +25,7 @@ const HEIGHT_CONDITIONS: Condition[] = [
   [compareRange(conditionalHeight, [31, 60]), "color('#E1CEE8')"],
   [compareRange(conditionalHeight, [12, 31]), "color('#F7FF00')"],
   [compareRange(conditionalHeight, [0, 12]), "color('#382A54')"],
+  DEFAULT_CONDITION,
 ];
 
 const conditionalPurpose = "用途";
@@ -45,6 +48,7 @@ const PURPOSE_CONDITIONS: Condition[] = [
   [equalString(conditionalPurpose, "防衛施設"), "color('#b22222')"],
   [equalString(conditionalPurpose, "その他"), "color('#d8bfd8')"],
   [equalString(conditionalPurpose, "不明"), "color('#e6e6fa')"],
+  DEFAULT_CONDITION,
 ];
 
 const conditionalStructure = "建物構造";
@@ -53,6 +57,7 @@ const STRUCTURE_CONDITIONS: Condition[] = [
   [equalString(conditionalStructure, "防火造"), "color('#bc8f8f')"],
   [equalString(conditionalStructure, "準防火造"), "color('#d6caae')"],
   [equalString(conditionalStructure, "木造"), "color('#d2b48c')"],
+  DEFAULT_CONDITION,
 ];
 
 const conditionalFireproof = "建物利用現況_耐火構造種別";
@@ -61,6 +66,7 @@ const FIREPROOF_CONDITIONS: Condition[] = [
   [equalString(conditionalFireproof, "準耐火造"), "color('#8e9bb0')"],
   [equalString(conditionalFireproof, "その他"), "color('#d083af')"],
   [equalString(conditionalFireproof, "不明"), "color('#89c2ea')"],
+  DEFAULT_CONDITION,
 ];
 
 export const makeSelectedFloodCondition = (
@@ -72,13 +78,14 @@ export const makeSelectedFloodCondition = (
         [equalNumber(propertyName, 2), "color('#fad4bc')"],
         [equalNumber(propertyName, 3), "color('#f5b2b1')"],
         [equalNumber(propertyName, 4), "color('#ef8c8c')"],
+        DEFAULT_CONDITION,
       ]
     : undefined;
 
 export const COLOR_TYPE_CONDITIONS: {
   [K in BaseFieldProps<"buildingColor">["value"]["colorType"]]: Condition[];
 } = {
-  none: [],
+  none: [DEFAULT_CONDITION],
   height: HEIGHT_CONDITIONS,
   purpose: PURPOSE_CONDITIONS,
   structure: STRUCTURE_CONDITIONS,
