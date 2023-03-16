4d7a46194862cdc1d6b52ae6c9b944ca42c95451
nourbalaha <nourbalaha0@gmail.com>
2023-03-16 23:12:32 +0900

    feat: add helper functions to color.ts

--- plugin/web/extensions/sidebar/utils/color.ts
+++ plugin/web/extensions/sidebar/utils/color.ts
@@ -6,3 +6,41 @@ export const getRGBAFromString = (rgbaStr: string | undefined): RGBA | undefined
 };
 
 export const rgbaToString = (rgba: RGBA) => `rgba(${rgba.join(",")})`;
+
+export const generateColorGradient = (
+  colorStart: string,
+  colorEnd: string,
+  colorCount: number,
+): string[] => {
+  const startRGB = hexToRgb(colorStart);
+  const endRGB = hexToRgb(colorEnd);
+
+  const stepR = (endRGB[0] - startRGB[0]) / (colorCount - 1);
+  const stepG = (endRGB[1] - startRGB[1]) / (colorCount - 1);
+  const stepB = (endRGB[2] - startRGB[2]) / (colorCount - 1);
+
+  const gradientColors = [];
+  for (let i = 0; i < colorCount; i++) {
+    const r = Math.round(startRGB[0] + stepR * i);
+    const g = Math.round(startRGB[1] + stepG * i);
+    const b = Math.round(startRGB[2] + stepB * i);
+    const color = rgbToHex([r, g, b]);
+    gradientColors.push(color);
+  }
+
+  return gradientColors;
+};
+
+export const hexToRgb = (hex: string): number[] => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return [r, g, b];
+};
+
+export const rgbToHex = (rgb: number[]): string => {
+  const r = rgb[0].toString(16).padStart(2, "0");
+  const g = rgb[1].toString(16).padStart(2, "0");
+  const b = rgb[2].toString(16).padStart(2, "0");
+  return `#${r}${g}${b}`;
+};
