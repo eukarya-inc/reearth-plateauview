4e9e6dd307ddc3fd705dfd2d677a45f0949f58f2
nourbalaha <nourbalaha0@gmail.com>
2023-03-17 15:47:38 +0900

    use tinycolor library

--- plugin/web/extensions/sidebar/utils/color.test.ts
+++ plugin/web/extensions/sidebar/utils/color.test.ts
@@ -1,30 +1,10 @@
 import { expect, test } from "vitest";
 
-import {
-  getRGBAFromString,
-  generateColorGradient,
-  hexToRgb,
-  rgbToHex,
-  colorToHex,
-  colorToRgb,
-  rgbToArray,
-  setColor,
-} from "./color";
+import { getRGBAFromString, generateColorGradient } from "./color";
 
 test("getRGBAFromString", () => {
   expect(getRGBAFromString("rgba(100, 24, 255, 1)")).toEqual([100, 24, 255, 1]);
   expect(getRGBAFromString("rgba(100,24,255,0.5)")).toEqual([100, 24, 255, 0.5]);
-});
-
-test("rgbToArray", () => {
-  expect(rgbToArray("rgb(255,0,0)")).toEqual([255, 0, 0]);
-  expect(rgbToArray("red")).toEqual([]);
-});
-
-test("setColor", () => {
-  expect(setColor("rgb(255,0,0)")).toEqual([255, 0, 0]);
-  expect(setColor("red")).toEqual([255, 0, 0]);
-  expect(setColor("#ff0000")).toEqual([255, 0, 0]);
 });
 
 test("generateColorGradient", () => {
@@ -35,35 +15,4 @@ test("generateColorGradient", () => {
     "#4000bf",
     "#0000ff",
   ]);
-});
-
-test("hexToRgb", () => {
-  expect(hexToRgb("#ff0000")).toEqual([255, 0, 0]);
-  expect(hexToRgb("#008000")).toEqual([0, 128, 0]);
-  expect(hexToRgb("#0000ff")).toEqual([0, 0, 255]);
-  expect(hexToRgb("red")).toEqual([0, 0, 0]);
-  expect(hexToRgb("something")).toEqual([0, 0, 0]);
-});
-
-test("rgbToHex", () => {
-  expect(rgbToHex([255, 0, 0])).toEqual("#ff0000");
-  expect(rgbToHex([0, 128, 0])).toEqual("#008000");
-  expect(rgbToHex([0, 0, 255])).toEqual("#0000ff");
-  expect(rgbToHex([])).toEqual("");
-  expect(rgbToHex([0, 1])).toEqual("");
-});
-
-test("colorToRgb", () => {
-  expect(colorToRgb("red")).toEqual([255, 0, 0]);
-  expect(colorToRgb("green")).toEqual([0, 128, 0]);
-  expect(colorToRgb("blue")).toEqual([0, 0, 255]);
-  expect(colorToRgb("something")).toEqual([0, 0, 0]);
-  expect(colorToRgb("")).toEqual([0, 0, 0]);
-});
-
-test("colorToHex", () => {
-  expect(colorToHex("red")).toEqual("#ff0000");
-  expect(colorToHex("green")).toEqual("#008000");
-  expect(colorToHex("blue")).toEqual("#0000ff");
-  expect(colorToHex("something")).toEqual("#000000");
 });
--- plugin/web/extensions/sidebar/utils/color.ts
+++ plugin/web/extensions/sidebar/utils/color.ts
@@ -12,30 +12,15 @@ export const rgbaToString = (rgba: RGBA) => `rgba(${rgba.join(",")})`;
 export const RGB_COLOR_REGEX = /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/;
 export const HEX_COLOR_REGEX = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 
-export const rgbToArray = (rgb: string): number[] => {
-  return RGB_COLOR_REGEX.test(rgb)
-    ? rgb
-        .replace(/^rgb\(|\s+|\)$/g, "")
-        .split(",")
-        .map(Number)
-    : [];
-};
-
-export const setColor = (color: string) => {
-  return HEX_COLOR_REGEX.test(color)
-    ? hexToRgb(color)
-    : RGB_COLOR_REGEX.test(color)
-    ? rgbToArray(color)
-    : colorToRgb(color);
-};
-
 export const generateColorGradient = (
   colorStart: string,
   colorEnd: string,
   colorCount: number,
 ): string[] => {
-  const startRGB = setColor(colorStart);
-  const endRGB = setColor(colorEnd);
+  const start = tinycolor(colorStart).toRgb();
+  const startRGB = [start.r, start.g, start.b];
+  const end = tinycolor(colorEnd).toRgb();
+  const endRGB = [end.r, end.g, end.b];
 
   const stepR = (endRGB[0] - startRGB[0]) / (colorCount - 1);
   const stepG = (endRGB[1] - startRGB[1]) / (colorCount - 1);
@@ -46,30 +31,9 @@ export const generateColorGradient = (
     const r = Math.round(startRGB[0] + stepR * i);
     const g = Math.round(startRGB[1] + stepG * i);
     const b = Math.round(startRGB[2] + stepB * i);
-    const color = rgbToHex([r, g, b]);
+    const color = tinycolor({ r, g, b }).toHexString();
     gradientColors.push(color);
   }
 
   return gradientColors;
-};
-
-export const hexToRgb = (hex: string): number[] => {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
-  return !isNaN(r) && !isNaN(g) && !isNaN(b) ? [r, g, b] : [0, 0, 0];
-};
-
-export const rgbToHex = (rgb: number[]): string => {
-  if (rgb.length < 3) return "";
-  const r = rgb[0].toString(16).padStart(2, "0");
-  const g = rgb[1].toString(16).padStart(2, "0");
-  const b = rgb[2].toString(16).padStart(2, "0");
-  return `#${r}${g}${b}`;
-};
-
-export const colorToRgb = (color: string): number[] => hexToRgb(colorToHex(color));
-
-export const colorToHex = (color: string): string => {
-  return tinycolor(color).toHexString();
 };
