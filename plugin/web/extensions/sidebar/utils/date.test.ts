79da829ee7b94689010fcd54cbdc766472e8044d
nourbalaha <nourbalaha0@gmail.com>
2023-03-27 10:38:08 +0900

    test: fix formatDateTime test case

--- plugin/web/extensions/sidebar/utils/date.test.ts
+++ plugin/web/extensions/sidebar/utils/date.test.ts
@@ -3,6 +3,8 @@ import { expect, test } from "vitest";
 import { formatDateTime } from "./date";
 
 test("formatDateTime", () => {
-  expect(formatDateTime("2023-03-25", "12:00:00")).toBe("2023-03-25T03:00:00.000Z");
+  expect(formatDateTime("2023-03-25", "12:00:00")).toBe(
+    new Date("2023-03-25T12:00:00").toISOString(),
+  );
   expect(formatDateTime("xxx", "yyy")).toBe(new Date().toISOString());
 });
