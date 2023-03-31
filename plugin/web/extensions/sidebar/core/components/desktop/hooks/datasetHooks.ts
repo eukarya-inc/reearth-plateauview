617915f13365050db4a84c702efb31679370468e
pyshx <piyushchauhan1004@gmail.com>
2023-04-01 02:47:40 +0530

    chore(plugin): refactor datasetHooks

--- plugin/web/extensions/sidebar/core/components/desktop/hooks/datasetHooks.ts
+++ plugin/web/extensions/sidebar/core/components/desktop/hooks/datasetHooks.ts
@@ -1,6 +1,6 @@
 import { Project } from "@web/extensions/sidebar/types";
 import { postMsg } from "@web/extensions/sidebar/utils";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 import { Data, DataCatalogItem, Template } from "../../../types";
 import { convertToData } from "../../utils";
@@ -42,34 +42,45 @@ export default ({
       const isNew = !data?.find(d => d.dataID === dataset.dataID);
 
       const fetchURL = !isNew
-        ? `${backendURL}/sidebar/${backendProjectName}/data/${dataset.id}` // should be id and not dataID because id here is the CMS item's id
+        ? `${backendURL}/sidebar/${backendProjectName}/data/${dataset.id}`
         : `${backendURL}/sidebar/${backendProjectName}/data`;
 
       const method = !isNew ? "PATCH" : "POST";
 
-      const res = await fetch(fetchURL, {
-        headers: {
-          authorization: `Bearer ${backendAccessToken}`,
-        },
-        method,
-        body: JSON.stringify(datasetToSave),
-      });
-      if (res.status !== 200) {
+      try {
+        const res = await fetch(fetchURL, {
+          headers: {
+            authorization: `Bearer ${backendAccessToken}`,
+          },
+          method,
+          body: JSON.stringify(datasetToSave),
+        });
+
+        if (res.status !== 200) {
+          throw new Error(`Failed to save dataset: ${res.statusText}`);
+        }
+
+        const savedDataset = await res.json();
+        console.log("Saved dataset: ", savedDataset);
         handleBackendFetch();
-        return;
+      } catch (error) {
+        console.error(error);
+        handleBackendFetch();
       }
-      const data2 = await res.json();
-      console.log("DATA JUST SAVED: ", data2);
-      handleBackendFetch(); // MAYBE UPDATE THIS LATER TO JUST UPDATE THE LOCAL VALUE
     },
     [data, templates, backendAccessToken, backendURL, backendProjectName, handleBackendFetch],
   );
+
+  useEffect(() => {
+    handleBackendFetch();
+  }, [handleBackendFetch]);
 
   const handleDatasetUpdate = useCallback(
     (updatedDataset: DataCatalogItem, cleanseOverride?: any) => {
       updateProject?.(project => {
         const updatedDatasets = [...project.datasets];
         const datasetIndex = updatedDatasets.findIndex(d2 => d2.dataID === updatedDataset.dataID);
+
         if (datasetIndex >= 0) {
           if (updatedDatasets[datasetIndex].visible !== updatedDataset.visible) {
             postMsg({
@@ -77,15 +88,19 @@ export default ({
               payload: { dataID: updatedDataset.dataID, hide: !updatedDataset.visible },
             });
           }
+
           if (cleanseOverride) {
             setCleanseOverride?.(cleanseOverride);
           }
+
           updatedDatasets[datasetIndex] = updatedDataset;
         }
+
         const updatedProject = {
           ...project,
           datasets: updatedDatasets,
         };
+
         postMsg({ action: "updateProject", payload: updatedProject });
         return updatedProject;
       });
@@ -94,22 +109,30 @@ export default ({
   );
 
   const handleDatasetSave = useCallback(
-    (dataID: string) => {
-      (async () => {
-        if (!inEditor) return;
-        setLoading?.(true);
-        const selectedDataset = project?.datasets.find(d => d.dataID === dataID);
+    async (dataID: string) => {
+      if (!inEditor) return;
 
+      setLoading?.(true);
+
+      const selectedDataset = project?.datasets.find(d => d.dataID === dataID);
+
+      if (!selectedDataset) return;
+
+      try {
         await handleDataRequest(selectedDataset);
-        setLoading?.(false);
-      })();
+      } catch (error) {
+        console.error(error);
+      }
+
+      setLoading?.(false);
     },
     [inEditor, project?.datasets, setLoading, handleDataRequest],
   );
 
   const handleDatasetPublish = useCallback(
-    (dataID: string, publish: boolean) => {
+    async (dataID: string, publish: boolean) => {
       if (!inEditor || !processedCatalog) return;
+
       const dataset = processedCatalog.find(item => item.dataID === dataID);
 
       if (!dataset) return;
@@ -119,31 +142,36 @@ export default ({
       updateProject?.(project => {
         const updatedDatasets = [...project.datasets];
         const datasetIndex = updatedDatasets.findIndex(d2 => d2.dataID === dataID);
+
         if (datasetIndex >= 0) {
           updatedDatasets[datasetIndex] = dataset;
         }
+
         return {
           ...project,
           datasets: updatedDatasets,
         };
       });
 
-      handleDataRequest(dataset);
+      try {
+        await handleDataRequest(dataset);
 
-      if (publish && publishToGeospatial && dataset.itemId && backendURL && backendAccessToken) {
-        fetch(`${backendURL}/publish_to_geospatialjp`, {
-          headers: {
-            authorization: `Bearer ${backendAccessToken}`,
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-          body: JSON.stringify({ id: dataset.itemId }),
-        })
-          .then(r => {
-            if (!r.ok)
-              throw `failed to publish the data on gspatial.jp: status code is ${r.statusText}`;
-          })
-          .catch(console.error);
+        if (publish && publishToGeospatial && dataset.itemId && backendURL && backendAccessToken) {
+          const res = await fetch(`${backendURL}/publish_to_geospatialjp`, {
+            headers: {
+              authorization: `Bearer ${backendAccessToken}`,
+              "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({ id: dataset.itemId }),
+          });
+
+          if (!res.ok) {
+            throw new Error(`Failed to publish dataset to geospatial.jp: ${res.statusText}`);
+          }
+        }
+      } catch (error) {
+        console.error(error);
       }
     },
     [
