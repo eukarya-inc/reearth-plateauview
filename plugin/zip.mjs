030e72ccb944e7ad170525aa04d9deb23b899490
Piyush Chauhan <42397980+pyshx@users.noreply.github.com>
2023-06-21 16:55:17 +0530

    ci(plugin): fix build-plugin workflow (#447)

--- plugin/zip.mjs
+++ plugin/zip.mjs
@@ -7,7 +7,7 @@ import Yml from "yml";
 const root = path.resolve("./");
 
 const pluginInfo = Yml.load(`${root}/public/reearth.yml`);
-const filename = `${pluginInfo.id}-${pluginInfo.version}.zip`;
+const filename = `${pluginInfo.id}.zip`;
 
 const output = fs.createWriteStream(`${root}/dist/${filename}`);
 const archive = archiver("zip", { zlib: { level: 9 } });
